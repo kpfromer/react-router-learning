42b5efdf8f9504237b30c3dd9bdb7c814aa435c5
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-24 22:47:31 -0600

    Refactor links to use NavLink for styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+// NavLink adds a .active class to the link when it is active
+// this allows for styling
+// activeStyle prop uses the object provided as active styling
+// activeClassName adds the className to the tag, when active (replaces .active)
 
 const Header = () => (
   <header>
     {/*<span className="icn-logo"><i className="material-icons">code</i></span>*/}
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/teachers">Teachers</Link></li>
-      <li><Link to="/courses">Courses</Link></li>
+      {/* NOTE: exact is needed here as well for a similar reason to App.js */}
+      {/* If it is not included it will all was have a .active class */}
+      <li><NavLink exact to="/" activeStyle={{ background: 'tomato' }}>Home</NavLink></li>
+      <li><NavLink to="/about" activeClassName="wowIamSoFit">About</NavLink></li>
+      <li><NavLink to="/teachers">Teachers</NavLink></li>
+      <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>    
   </header>
 );
