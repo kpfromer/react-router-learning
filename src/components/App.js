38dff34ec68602ea9dff4de869d94f9891770d89
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-25 00:53:58 -0600

    Add 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./Home";
 import About from "./About";
 import Header from "./Header";
 import Courses from "./Courses";
 import Teachers from "./Teachers";
+import NotFound from "./NotFound";
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
       {/* With out the exact prop the /about component will also be rendered */}
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About}/>
-      <Route path="/courses" component={Courses} />
-      {/* You can use render and pass a function! useful for passing props */}
-      <Route path="/teachers" render={() => <Teachers />} />
+
+      {/* Switch is basically express type 404 page */}
+      {/* First url that matches will load */}
+      {/* if none match fallback is the NotFound route */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About}/>
+        <Route path="/courses" component={Courses} />
+        {/* You can use render and pass a function! useful for passing props */}
+        <Route path="/teachers" render={() => <Teachers />} />
+
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
