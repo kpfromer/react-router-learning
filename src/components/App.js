30bab62aef0bf1d9ba8b1e93ca312e843735701a
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-24 22:34:44 -0600

    Add basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Header from "./Header";
+import Courses from "./Courses";
+import Teachers from "./Teachers";
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      {/* With out the exact prop the /about component will also be rendered */}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About}/>
+      <Route path="/courses" component={Courses} />
+      {/* You can use render and pass a function! useful for passing props */}
+      <Route path="/teachers" render={() => <Teachers />} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
